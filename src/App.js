6db3a0e3778e19aee54c8d87fe3eeab794ae5751
garpRojas <grojaspineda@gmail.com>
2022-02-18 08:23:55 -0600

    Adding new route functionality on diferent menu items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 
+import {Route, Switch} from "react-router-dom";
+
 import HomePage from './pages/homepage.component';
+
+const Hats = () => (
+  <div>
+    <h1>Hello Hats</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={Hats} />
+
     </div>
   );
 }
