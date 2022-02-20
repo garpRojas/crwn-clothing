c3f18e016fad163dbd3bf92cb0106b3929cc2e3c
garpRojas <grojaspineda@gmail.com>
2022-02-20 06:19:39 -0600

    Adding Routing to shop example

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 import {Route, Switch} from "react-router-dom";
 
-import HomePage from './pages/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const Hats = () => (
   <div>
@@ -15,7 +16,8 @@ function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/hats" component={Hats} />
+      <Route exact path="/shop" component={ShopPage} />
+
 
     </div>
   );
